fbe892e3dedeeec7e0ab82bdc2b7a4704e7299da
Masami_Nakaoka <neti0326@gmail.com>
2020-05-17 07:58:45 +0000

    first commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+    pattern: String,
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
+fn main() {
+    // let pattern = std::env::args().nth(1).expect("no pattern given");
+    // let path = std::env::args().nth(2).expect("no path given");
+    // let args = Cli {
+    //     pattern: pattern,
+    //     path: std::path::PathBuf::from(path),
+    // };
+    let args = Cli::from_args();
+}
